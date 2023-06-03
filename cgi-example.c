1b25745b6f28534e58ef43ece95d6c7d8ae89d35
Christoph <christoph@debian>
2023-06-03 20:46:45 +0200

    read important request data

--- cgi-example.c
+++ cgi-example.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+//The method of the call
+char *method;
+
+//The query data
+char *query;
+
+//The request data
+char *content_length;
+char *body;
+
 
 int main(){
     printf("Content-type: text/html\n\n");
-    printf("Hello, World.");
+
+
+    //read the method of the request
+    method = getenv("REQUEST_METHOD");
+    //read the query data
+    query = getenv("QUERY_STRING");
+
+    if((content_length = getenv("CONTENT_LENGTH")) != NULL){
+        //allocate the lenght of the body
+        body = malloc(sizeof(char)*atoi(content_length));
+        //read body
+        for(int c = 0; c < atoi(content_length); c++){
+            *(body+c) = fgetc(stdin);
+        }
+    }
+
+    printf(method);
+    printf(query);
+    printf(body);
 }
